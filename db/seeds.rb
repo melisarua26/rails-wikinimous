0c15f62aad4326eb8feee4cbf680f38622f7755d
Melisa Rua <melisa.2610@hotmail.com>
2022-09-11 11:49:31 -0500

    Wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,63 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+article1 = Article.new(
+  title: 'Pao was here',
+  content: 'Asdf'
+)
+article1.save!
+
+article2 = Article.new(
+  title: 'Hello world',
+  content: "Very good!"
+)
+article2.save!
+
+article3 = Article.new(
+  title: 'Queen elizabeth',
+  content: "Died"
+)
+article3.save!
+
+article4 = Article.new(
+  title: 'Front-line 24/7 definition',
+   content: "Consequuntur culpa ad non."
+)
+article4.save!
+
+article5 = Article.new(
+  title: 'Harry Potter',
+   content: "Movie"
+)
+article5.save!
+
+article6 = Article.new(
+  title: 'TheNewYork Post',
+   content: "Breaking News"
+)
+article6.save!
+
+article7 = Article.new(
+  title: 'Face to face discrete application',
+  content: "Sit nulla omnis saepe Et asperiores."
+)
+article7.save!
+
+article8 = Article.new(
+  title: 'Batch 912',
+  content: "Vamos con toda"
+)
+article8.save!
+
+article9 = Article.new(
+  title: 'Task',
+   content: "Completed"
+  )
+  article9.save!
+
+  article10 = Article.new(
+    title: 'Colombia',
+    content: "Is fun"
+    )
+  article10.save!
